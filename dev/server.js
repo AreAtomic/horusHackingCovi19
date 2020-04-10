4393e6b4b7a86159c53153cb96ac5a9875501ac3
Areatomic <aureliensebe@gmail.com>
2020-04-10 16:34:37 +0200

    Initilisation du côté client

--- dev/server.js
+++ dev/server.js
@@ -3,3 +3,13 @@ const connectDB = require('./config/db');
 const path = require('path');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

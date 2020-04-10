e3759c8f1cedb198e9da72124dd1be752323275e
Areatomic <aureliensebe@gmail.com>
2020-04-10 17:38:56 +0200

    Exemple de requête d'autenthification

--- dev/server.js
+++ dev/server.js
@@ -10,6 +10,9 @@ connectDB();
 // Init Middleware
 app.use(express.json());
 
+app.use('/api/users', require('./routes/user'));
+app.use('/api/auth', require('./routes/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

a47ce1bd390e9d9dfa50a66bfec50e2ba1a01710
Areatomic <aureliensebe@gmail.com>
2020-04-12 02:04:18 +0200

    Email inscription

--- site-vitrine/src/Components/form/form.js
+++ site-vitrine/src/Components/form/form.js
@@ -1,32 +1,67 @@
 import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 import '../../App.css';
+import * as emailjs from 'emailjs-com';
+import {
+  Button,
+  FormFeedback,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from 'reactstrap';
+
+function sendEmail(e) {
+  e.preventDefault();
+
+  emailjs.sendForm('gmail', 'omiz', e.target, 'user_ZxyoPZfkQ5PWeOtPX8BVy')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+}
 
 class form extends Component {
   render() {
     return (
       <Fragment>
-        <Form action='/send'>
-          <input type='text' placeholder='Nom' className='signupRows'></input>
+        <Form onSubmit={sendEmail} className="formulaireInscription">
           <input
             type='text'
-            placeholder='Prenom'
+            name='name'
+            // value={this.state.name}
+            // onChange={this.handleChange.bind(this, 'name')}
+            placeholder='Nom'
             className='signupRows'
             required='true'
           ></input>
           <input
             type='text'
+            name='prenom'
+            placeholder='Prenom'
+            className='signupRows'
+            // value={this.state.prenom}
+            // onChange={this.handleChange.bind(this, 'prenom')}
+            required='true'
+          ></input>
+          <input
+            type='text'
+            name='birthday'
             placeholder='Date de naissance'
             className='signupRows'
             required='true'
           ></input>
           <input
             type='email'
+            name='email'
             placeholder='Email'
             className='signupRows'
             required='true'
+            // onChange={this.handleChange.bind(this, 'email')}
+            // value={this.state.email}
           ></input>
-          <input type='submit' value='Inscrivez-vous' className='button' />
+          <Button type='submit' value='Inscrivez-vous' className='button' />
         </Form>
       </Fragment>
     );
@@ -35,8 +70,8 @@ class form extends Component {
 
 export default form;
 
-const Form = styled.form({
+/*const Form = styled.form({
   display: 'grid',
   width: '100%',
   marginBottom: '50px',
-});
+});*/

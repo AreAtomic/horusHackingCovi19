630a9b8b38ed82df35c7d390dbafecd5d08c5f2a
Camille <ca.carbasse@orange.fr>
2020-04-12 10:51:14 +0200

    Fin du site vitrine

--- site-vitrine/src/Components/Nav.js
+++ site-vitrine/src/Components/Nav.js
@@ -8,9 +8,10 @@ class Nav extends Component {
   render() {
     return (
       <Header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo'/>
+        <Link to='/'><img src={logo} className='App-logo' alt='logo'/></Link>
         <Links>
-          <Link to='/' style={{margin: '0 auto'}}>Plus d'infos</Link>
+        <a href ='https://forms.gle/Z26HQF1R9k3PqHUf9'style={{margin: '0 auto'}}>Formulaire</a>
+          <a href='https://hackcovid19.bemyapp.com/#/projects/5e90c489f1e9c6001bd9bdfc' style={{margin: '0 auto'}}>Plus d'infos</a>
           <Link to='/contact' style={{margin: '0 auto'}}>Nous contacter</Link>
         </Links>
       </Header>
@@ -22,7 +23,7 @@ export default Nav;
 
 const Header = styled.div({
   display: 'grid',
-  gridTemplateColumns: '10fr 2fr',
+  gridTemplateColumns: '10fr 3fr',
 });
 
 const Links = styled.div({
--- site-vitrine/src/Components/contact.js
+++ site-vitrine/src/Components/contact.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch,Link } from 'react-router-dom';
 import styled from 'styled-components';
 import logo from '../logo.png';
 import facebook from '../facebook.svg';
+import linkedin from '../LinkedIN.svg';
 import instagram from '../instagram.svg';
 import twitter from '../twitter.svg';
 import email from '../email.svg';
@@ -13,10 +14,11 @@ class App extends Component {
     return (
       <Fragment>
         <Resau>
-          <img src={facebook} alt='Facebook' className='reseau' />
-          <img src={instagram} alt='Facebook' className='reseau' />
-          <img src={email} alt='Facebook' className='reseau' />
-          <img src={twitter} alt='Facebook' className='reseau' />
+          <a href ='https://www.facebook.com/omiz.tousenlive/' className='reseau'><img src={facebook} alt='Facebook' className='reseau' /></a>
+          <a href='https://www.linkedin.com/company/65351414/admin/' className='reseau'><img src={linkedin} alt='LikedIn' className='reseau' /></a>
+          <a href='https://www.instagram.com/omiz.toutenlive/' className='reseau'><img src={instagram} alt='Instagram' className='reseau' /></a>
+          <a href='mailto:contact.omiz.startup@gmail.com' className='reseau'><img src={email} alt='Email' className='reseau' /></a>
+          <a href = 'https://twitter.com/en_omiz ' className='reseau'><img src={twitter} alt='Twitter' className='reseau' /></a>
         </Resau>
       </Fragment>
     );
@@ -27,7 +29,7 @@ export default App;
 
 const Resau = styled.div({
   display: 'grid',
-  gridTemplateColumns: 'repeat(4, 1fr)',
+  gridTemplateColumns: 'repeat(5, 1fr)',
   gridAutoRows: 'auto',
   marginTop: '15%',
 });
